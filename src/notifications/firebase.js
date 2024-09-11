d85147f978d6c092564bd5c8b86677826b6995ff
AtharvaDPavesoft <atharvadhamkepavesoft@gmail.com>
2024-09-11 14:19:37 +0530

    Updated firebase.js

--- src/notifications/firebase.js
+++ src/notifications/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATrbJoJP0yrUy10JEO-nS-00eXXsCqiQM",
-  authDomain: "react-pushnotifications-1.firebaseapp.com",
-  projectId: "react-pushnotifications-1",
-  storageBucket: "react-pushnotifications-1.appspot.com",
-  messagingSenderId: "258384220882",
-  appId: "1:258384220882:web:b5653f2d8804ae6ced130b",
-  measurementId: "G-GCC12ETRPH",
+  apiKey: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_API_KEY",
+  authDomain: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_AUTH_DOMAIN",
+  projectId: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_PROJECT_ID",
+  storageBucket: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_STORAGE_BUCKET",
+  messagingSenderId: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_APP_ID",
+  measurementId: "REPLACE_WITH_YOUR_FIREBASE_MESSAGING_MEASUREMENT_ID",
 };
 
 // Initialize Firebase
@@ -23,7 +23,7 @@ export const generateToken = async () => {
   if (permission === "granted") {
     const token = await getToken(messaging, {
       vapidKey:
-        "BCeVFir6FEoMffD1AAgG1EYkEPStf3zap6Icd2uV73hr5Mtl0DYDIAH8kKLlmVg_cZc4tIH_-OUuXO6Mp9a_4U4",
+        "REPLACE_WITH_YOUR_VAPID_KEY",
     });
     console.log(token);
   }
